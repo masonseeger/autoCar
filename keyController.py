51b27887767684f36a781b6976e8a090625b2503
Mason <masonseeger_2019@depauw.edu>
2018-10-23 12:25:04 -0400

    Updated keyController with autonomous driving functions, some kinks still need to be worked out.

--- keyController.py
+++ keyController.py
@@ -1,4 +1,8 @@
 import time
+import numpy as np
+import math
+from statistics import *
+from rplidar import RPLidar
 import RPi.GPIO as GPIO
 import board
 import busio
@@ -10,11 +14,18 @@ sp = 0
 aligned = 115
 br = True
 cPos = 0
+#circle = np.zeros(360)
+obstacle = []
+freeSpace = [1,1,1,1,1,1]
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.nodelay(True)
 
 try:
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
+    lidar = RPLidar("/dev/ttyUSB0")
+    time.sleep(1)
 
     i2c = busio.I2C(board.SCL, board.SDA)
     pca = adafruit_pca9685.PCA9685(i2c)
@@ -29,8 +40,9 @@ try:
     steer.angle = aligned
     speed.throttle = 0
     sp = 0
-except:
+except Exception as e:
     print("something went wrong initializing...")
+    print(e)
     curses.endwin()
     br = False
 
@@ -43,9 +55,142 @@ def still():
 
 #makes sure that I can travel in reverse
 def setLow():
-    speed.throttle= -.5
+    speed.throttle = -.5
+    time.sleep(.05)
     speed.throttle = 0
+    time.sleep(.05)
 
+def leftBackTurn():
+    #stdscr.addstr(cPos,0,"reversing left turn")
+    #cPos+=1
+    steer.angle = 160
+    setLow()
+    speed.throttle = -.3
+    time.sleep(1)
+
+    steer.angle = aligned
+    speed.throttle = .2
+    time.sleep(.2)
+    still()
+
+def rightBackTurn():
+    #stdscr.addstr(cPos,0,"reversing right turn")
+    #cPos+=1
+    steer.angle = 55
+    setLow()
+    speed.throttle = -.3
+    time.sleep(1)
+
+    steer.angle = aligned
+    speed.throttle = .2
+    time.sleep(.2)
+    still()
+
+def autoSteer(sectors):
+
+    try:
+        sectorMins = [0,0,0,0,0,0]
+        sectorMean = [0,0,0,0,0,0]
+        j = 0
+        for i in sectors:
+            sectorMins[j] = min(i)
+            sectorMean[j] = mean(i)
+            j+=1
+        newAngle = (sectorMean[5] - sectorMean[1])
+        if newAngle>4:
+            newAngle = 4
+        elif newAngle<-4:
+            newAngle = -4
+        #print(newAngle)
+        if newAngle>=0 and sectorMins[5]>1:
+            steer.angle = aligned + (math.sqrt(newAngle))*20
+        elif sectorMins[1]>1:
+            steer.angle = aligned -  math.sqrt(abs(newAngle))*20
+        else:
+            steer.angle = aligned
+    except Exception as e:
+        print(e)
+        print("autoSteer")
+
+def autoSpeed(sectors):
+    try:
+        sectorMins = [0,0,0,0,0,0]
+        sectorMean = [0,0,0,0,0,0]
+        j=0
+        for i in sectors:
+            sectorMins[j] = min(i)
+            sectorMean[j] = mean(i)
+        if sectorMins[0]<.5:
+            still()
+            if sectorMins[5]>.5 and sectorMean[5]>sectorMean[1]:
+                rightBackTurn()
+            elif sectorMins[1]>.5:
+                leftBackTurn()
+        else:
+            speed.throttle = .23
+    except Exception as e:
+        print(e)
+        print('AutoSpeed')
+        still()
+
+
+def lidarDistanceAndSector(scan):
+    check = 0
+    try:
+        obstacles = []
+        sectors = [[],[],[],[],[],[]]
+        sector = 0
+        for j in scan:
+            dist = j[2]/1000
+            deg = math.floor(j[1])
+            obstacles.append([dist, deg])
+            if deg>=330 or deg<=30:
+                sector = 0
+            else:
+                sector = ((deg-30)//60)+1
+            #print(str(deg) + '   ' + str(sector))
+            sectors[sector].append(dist)
+        #mean the inside of the sectors
+        return(obstacles, sectors)
+
+    except Exception as e:
+        print(e)
+        print("error in LidarDandS")
+        #print(check)
+        return([], [])
+
+def autoDrive():
+    #stdscr.addstr(cPos, 0, 'Now driving autonomously')
+    #cPos+=1
+    keyInterrupt = stdscr.getch()
+    while keyInterrupt == -1:
+        keyInterrupt = stdscr.getch()
+        obstacles, sectors = lidarDistanceAndSector(next(iterator))
+        if obstacles:
+            autoSpeed(sectors)
+            autoSteer(sectors)
+    still()
+
+def lidarObsDistances(scan):
+    try:
+        obstacle = []
+        for j in scan:
+            dist = j[2]/1000
+            deg = math.floor(j[1])
+            #print("distance of " +str(dist)+"m at " + str(deg) + " degrees")
+            if dist <1:
+                #print("adding obstacle")
+                obstacle.append([dist, deg])
+                freeSpace[deg//60] = 0
+
+        #print(obstacle)
+        return(obstacle, 1)
+
+    except:
+        print("error in function")
+        return([], 0)
+
+iterator = lidar.iter_scans()
 try:
     #print("This is what you are looking for:::  "+str(speed.throttle))
     while br:
@@ -66,7 +211,7 @@ try:
         elif press == 100: #d
             stdscr.addstr(cPos,0,"Turning right, servo at " + str(steer.angle) + " degrees")
             cPos+=1
-            if steer.angle>70:
+            if steer.angle>55:
                 steer.angle -= 5
         elif press == 115: #s
             stdscr.addstr(cPos,0,"Decreasing velocity, throttle at " + str(speed.throttle))
@@ -87,10 +232,7 @@ try:
             cPos+=1
             if speed.throttle>0:
                 sp = 0
-                speed.throttle = -.5
-                time.sleep(.05)
-                speed.throttle = 0
-                time.sleep(.05)
+                setLow()
                 speed.throttle = -.5
                 time.sleep(.3)
                 speed.throttle = 0.001
@@ -102,56 +244,69 @@ try:
         elif press == 108: #l(eft)
             stdscr.addstr(cPos,0,"reversing left turn")
             cPos+=1
-            steer.angle = 160
-            speed.throttle = -.3
-            time.sleep(1.6)
-
-            steer.angle = aligned
-            speed.throttle = .2
-            time.sleep(.2)
-
+            leftBackTurn()
             still()
+
         elif press == 114: #r(ight)
             stdscr.addstr(cPos,0,"reversing right turn")
             cPos+=1
-            steer.angle = 70
-            speed.throttle = -.3
-            time.sleep(1.5)
-
-            steer.angle = aligned
-            speed.throttle = .2
-            time.sleep(.2)
-
+            rightBackTurn()
             still()
-
 
         elif press == 32: #space
             stdscr.addstr(cPos,0,"Stopping motor and realigning wheels")
+            setLow()
             cPos+=1
             still()
-except:
+
+        elif press == 103: #g(o into auto)
+            autoDrive()
+
+        freeSpace = [1,1,1,1,1,1]
+        obstacle, br = lidarObsDistances(next(iterator))
+        #if obstacle:
+            #j = 0
+            #for i in freeSpace:
+                #stdscr.addstr(cPos,0, "Watch Out! Obstacle in Sector " + str(i))
+                #cPos+=1
+                #if cPos>30:
+                #    stdscr.clear()
+                #    cPos = 0
+                #j+=1
+        #stdscr.addstr(cPos, 0 , "obs count: " + str(len(obstacle)) + ' ' + str(bool(not(1 in freeSpace))))
+        #cPos +=1
+
+
+        if not(1 in freeSpace):
+            stdscr.nodelay(False)
+            stdscr.addstr(cPos,0,"WARNING, ALL ROUTES BLOCKED, NEED USER ASSISTANCE!!!")
+            stdscr.addstr(cPos+1, 0, "Press any key when area is clear")
+            still()
+            press = stdscr.getch()
+            cPos +=2
+            stdscr.nodelay(True)
+
+
+except Exception as e:
+    print(e)
     curses.endwin()
     speed.throttle = 0
     time.sleep(.01)
     still()
     pca.deinit()
+    print("Something broke")
 
 pca.deinit()
 curses.endwin()
 GPIO.cleanup()
+lidar.stop()
+lidar.stop_motor()
+lidar.disconnect()
+
+if not(br):
+    print('hi')
 
 '''
     Could be helpful to make a function for braking based on how fast the car is already going.
-
-    Reverse problems may be a part of safety features of the esc...
-you may need to go into reverse, neutral, then reverse again in order to get things going...
-not sure but it could be helpful
-
-    Make a sort of set low function to automatically be put into reverse
-if not already. It is a safety feature of the ESC
-
-    Get rid of the stupid sp variable, it doesn't do its job very well.
-
-
     todo: clean up prints; remove sp; make more function calls; etc
 '''
--- lidarTst.py
+++ lidarTst.py
@@ -3,32 +3,58 @@ import numpy as np
 import time
 import math
 
-circle = np.zeros(360)
-
-lidar = RPLidar("/dev/ttyUSB0")
-time.sleep(1)
-
-#try:
-info = lidar.get_info()
-print(info)
-
-health = lidar.get_health()
-print(health)
-
-try:
-    for i, scan in enumerate(lidar.iter_scans()):
-        print('%d: Got %d measurements' % (i,len(scan)))
+def lidarDistances(scan):
+    try:
+        obstacle = []
+        print('Got %d measurements' % (len(scan)))
         for j in scan:
             dist = j[2]/1000
             deg = math.floor(j[1])
-            print("distance of " +str(dist)+"m at " + str(deg) + " degrees")
-except:
+            #print("distance of " +str(dist)+"m at " + str(deg) + " degrees")
+            if dist <1:
+                print("adding obstacle")
+                obstacle.append([dist, deg])
+
+        print(obstacle)
+        return(obstacle, 1)
+
+    except:
+        print("error in function")
+        return([], 0)
+
+def main():
+    circle = np.zeros(360)
+
+    lidar = RPLidar("/dev/ttyUSB1")
+    time.sleep(1)
+
+    go = 1
+
+    #try:
+    info = lidar.get_info()
+    print(info)
+
+    health = lidar.get_health()
+    print(health)
+
+    iterator = lidar.iter_scans()
+    obstacle = []
+    try:
+        while go:
+            obstacle, go = lidarDistances(next(iterator))
+            print("made it past return")
+            if obstacle:
+                print("obstacle returned true")
+                for i in obstacle:
+                    print("Watch Out! Obstacle " + str(i[0]) + "m away at " + str(i[1]) + " degrees.")
+            print("made it past if")
+    except:
+        print("error occured in main")
+
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
-    exit()
 
 
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
+
+main()
